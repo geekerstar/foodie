6bd32819bd8c3e1a470d7fd61504d0c25688c1df
geekerstar <247507792@qq.com>
2019-11-16 20:52:04 +0800

    完成数据层准备工作

---
+++ foodie-api/src/main/java/com/geekerstar/mapper/Application.java
@@ -0,0 +1,16 @@
+package com.geekerstar.mapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author geekerstar
+ * date: 2019/11/16 20:18
+ * description:
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
