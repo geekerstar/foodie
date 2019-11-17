ec15769ce3e424e45b48a106f1d02dc3404fdca4
geekerstar <247507792@qq.com>
2019-11-17 11:59:11 +0800

    mapper注解

--- foodie-mapper/src/main/java/com/geekerstar/mapper/StuMapper.java
+++ foodie-mapper/src/main/java/com/geekerstar/mapper/StuMapper.java
@@ -2,6 +2,8 @@ package com.geekerstar.mapper;
 
 import com.geekerstar.my.mapper.MyMapper;
 import com.geekerstar.pojo.Stu;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StuMapper extends MyMapper<Stu> {
 }
