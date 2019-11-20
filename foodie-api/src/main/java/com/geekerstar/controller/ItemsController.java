ad6f76c2bd96561df7724d7bb9b936ad7c1ce9f7
geekerstar <247507792@qq.com>
2019-11-20 20:06:28 +0800

    商品详情开发

---
+++ foodie-api/src/main/java/com/geekerstar/controller/ItemsController.java
@@ -0,0 +1,57 @@
+package com.geekerstar.controller;
+
+import com.geekerstar.pojo.Items;
+import com.geekerstar.pojo.ItemsImg;
+import com.geekerstar.pojo.ItemsParam;
+import com.geekerstar.pojo.ItemsSpec;
+import com.geekerstar.service.ItemService;
+import com.geekerstar.util.JSONResult;
+import com.geekerstar.vo.ItemInfoVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author geekerstar
+ * date: 2019/11/20 19:17
+ * description:
+ */
+@Api(value = "商品接口", tags = {"商品信息展示的相关接口"})
+@RestController
+@RequestMapping("items")
+public class ItemsController {
+    @Autowired
+    private ItemService itemService;
+
+    @ApiOperation(value = "查询商品详情", notes = "查询商品详情", httpMethod = "GET")
+    @GetMapping("/info/{itemId}")
+    public JSONResult info(
+            @ApiParam(name = "itemId", value = "商品id", required = true)
+            @PathVariable String itemId) {
+
+        if (StringUtils.isBlank(itemId)) {
+            return JSONResult.errorMsg(null);
+        }
+
+        Items item = itemService.queryItemById(itemId);
+        List<ItemsImg> itemImgList = itemService.queryItemImgList(itemId);
+        List<ItemsSpec> itemsSpecList = itemService.queryItemSpecList(itemId);
+        ItemsParam itemsParam = itemService.queryItemParam(itemId);
+
+        ItemInfoVO itemInfoVO = new ItemInfoVO();
+        itemInfoVO.setItem(item);
+        itemInfoVO.setItemImgList(itemImgList);
+        itemInfoVO.setItemSpecList(itemsSpecList);
+        itemInfoVO.setItemParams(itemsParam);
+
+        return JSONResult.ok(itemInfoVO);
+    }
+}
---
+++ foodie-service/src/main/java/com/geekerstar/service/impl/ItemServiceImpl.java
@@ -0,0 +1,70 @@
+package com.geekerstar.service.impl;
+
+import com.geekerstar.mapper.ItemsImgMapper;
+import com.geekerstar.mapper.ItemsMapper;
+import com.geekerstar.mapper.ItemsParamMapper;
+import com.geekerstar.mapper.ItemsSpecMapper;
+import com.geekerstar.pojo.Items;
+import com.geekerstar.pojo.ItemsImg;
+import com.geekerstar.pojo.ItemsParam;
+import com.geekerstar.pojo.ItemsSpec;
+import com.geekerstar.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+
+/**
+ * @author geekerstar
+ * date: 2019/11/20 19:21
+ * description:
+ */
+public class ItemServiceImpl implements ItemService {
+
+    @Autowired
+    private ItemsMapper itemsMapper;
+    @Autowired
+    private ItemsImgMapper itemsImgMapper;
+    @Autowired
+    private ItemsSpecMapper itemsSpecMapper;
+    @Autowired
+    private ItemsParamMapper itemsParamMapper;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public Items queryItemById(String itemId) {
+        return itemsMapper.selectByPrimaryKey(itemId);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<ItemsImg> queryItemImgList(String itemId) {
+        Example itemsImgExp = new Example(ItemsImg.class);
+        Example.Criteria criteria = itemsImgExp.createCriteria();
+        criteria.andEqualTo("itemId", itemId);
+
+        return itemsImgMapper.selectByExample(itemsImgExp);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<ItemsSpec> queryItemSpecList(String itemId) {
+        Example itemsSpecExp = new Example(ItemsSpec.class);
+        Example.Criteria criteria = itemsSpecExp.createCriteria();
+        criteria.andEqualTo("itemId", itemId);
+
+        return itemsSpecMapper.selectByExample(itemsSpecExp);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public ItemsParam queryItemParam(String itemId) {
+        Example itemsParamExp = new Example(ItemsParam.class);
+        Example.Criteria criteria = itemsParamExp.createCriteria();
+        criteria.andEqualTo("itemId", itemId);
+
+        return itemsParamMapper.selectOneByExample(itemsParamExp);
+    }
+}
